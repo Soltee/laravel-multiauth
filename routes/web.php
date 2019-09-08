b21e4c50d529cfd1427993c959ed48788a0742b5
unknown <soltee.13@gmail.com>
2019-09-08 18:01:44 +0545

    Again

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,27 @@ Route::get('/', 'WelcomeController@index')->name('welcome');
 Auth::routes();
 
 //Admin
-Route::get('admin/register', 'Admin\RegisterController@showRegisterationForm')->name('admin.register.view');
-Route::post('admin/register', 'Admin\RegisterController@register')->name('admin.register');
+Route::get(/'admin/register', 'Admin\RegisterController@showRegisterationForm')->name('admin.register.view');
+Route::post('/admin/register', 'Admin\RegisterController@register')->name('admin.register');
 
-Route::get('admin/verify', 'Admin\VerificationController@show')->name('admin.verify');
+Route::get('/admin/verify', 'Admin\VerificationController@show')->name('admin.verify');
 
-Route::get('admin', 'Admin\LoginController@showLoginForm')->name('admin.login');
-Route::post('admin', 'Admin\LoginController@login');
+Route::get('/admin', 'Admin\LoginController@showLoginForm')->name('admin.login');
+Route::post('/admin', 'Admin\LoginController@login');
 
-Route::get('admin-password/reset', 'Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
-Route::post('admin-password/email', 'Admin\ForgotPasswordController@sendAdminResetLinkEmail')->name('admin.password.email.send');
+Route::get('/admin-password/reset', 'Admin\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+Route::post('/admin-password/email', 'Admin\ForgotPasswordController@sendAdminResetLinkEmail')->name('admin.password.email.send');
 
-Route::get('admin-password/reset/{token}', 'Admin\ResetPasswordController@showResetForm')->name('admin.password.reset');
-Route::post('admin-password/reset', 'Admin\ResetPasswordController@reset')->name('admin.password.update');
+Route::get('/admin-password/reset/{token}', 'Admin\ResetPasswordController@showResetForm')->name('admin.password.reset');
+Route::post('/admin-password/reset', 'Admin\ResetPasswordController@reset')->name('admin.password.update');
 
 
 //Admin EMaail veify
-Route::get('admin/email/verify', 'Admin\VerificationController@show')->name('admin.verification.notice');
-Route::get('admin/email/verify/{id}', 'Admin\VerificationController@verify')->name('admin.verification.verify');
-Route::get('admin/email/resend', 'Admin\VerificationController@resend')->name('admin.verification.resend');
+Route::get('/admin/email/verify', 'Admin\VerificationController@show')->name('admin.verification.notice');
+Route::get('/admin/email/verify/{id}', 'Admin\VerificationController@verify')->name('admin.verification.verify');
+Route::get('/admin/email/resend', 'Admin\VerificationController@resend')->name('admin.verification.resend');
 
-Route::get('home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 
 
 
@@ -37,7 +37,7 @@ Route::post('/user/{user}/update', 'UsersController@update')->name('profile.upda
 Route::post('/user/{user}/delete', 'UsersController@destroy')->name('user.profile.delete')->middleware('auth:admin');
 
 //Admin
-Route::get('admin/home', 'AdminController@index')->name('admin.home')->middleware('verified');
+Route::get('/admin/home', 'AdminController@index')->name('admin.home')->middleware('verified');
 Route::get('/admin/profile', 'AdminController@show')->name('admin.profile');
 Route::post('/admin/{admin}/update', 'AdminController@update')->name('admin.profile.update');
 
