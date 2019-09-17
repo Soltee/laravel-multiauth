3dec877a0f9695cc1346da659a7465e5b6663f1a
unknown <soltee.13@gmail.com>
2019-09-17 15:52:33 +0545

    Made some changes.

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,20 +6,11 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+
     public function index()
     {
         return view('welcome');
     }
+    
 }
